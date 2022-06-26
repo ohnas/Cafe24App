3ad8dd9fd2e6c6016561bebe498b77866c929234
ohnas <74810513+ohnas@users.noreply.github.com>
2022-06-26 18:40:42 +0900

    #3

--- cafe24app/__init__.py
+++ cafe24app/__init__.py
@@ -3,6 +3,7 @@ from .views import main_views
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_envvar('APP_CONFIG_FILE')
 
     app.register_blueprint(main_views.bp)
 
---
+++ config/production.py
@@ -0,0 +1,3 @@
+from config.default import *
+
+SECRET_KEY = b'Zb3\x81\xdb\xf1\xd9\xd7-Knb\x8eB\xa5\x18'
